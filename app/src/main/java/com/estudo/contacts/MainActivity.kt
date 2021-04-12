6f7f83bdd48f9f038838b369fc2429dac9f0af88
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-04-12 14:28:13 -0300

    Adiciona menu e cria recursos de strings para o mesmo

--- app/src/main/java/com/estudo/contacts/MainActivity.kt
+++ app/src/main/java/com/estudo/contacts/MainActivity.kt
@@ -2,6 +2,10 @@ package com.estudo.contacts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.estudo.contacts.model.Contact
@@ -26,15 +30,45 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateList() {
         adapter.updateList(
-            arrayListOf(Contact(
-                "Wemerson Fernandes",
-                "(31) 99999-9999",
-                "img.png"
-            ), Contact(
-                "Debora Cristina",
-                "(31) 99999-9999",
-                "img.png"
-            ))
+            arrayListOf(
+                Contact(
+                    "Wemerson Fernandes",
+                    "(31) 99999-9999",
+                    "img.png"
+                ), Contact(
+                    "Debora Cristina",
+                    "(31) 99999-9999",
+                    "img.png"
+                )
+            )
         )
+    }
+
+    private fun showToast(messege: String) {
+        Toast.makeText(this, messege, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       return when(item.itemId) {
+           R.id.item_menu_1 -> {
+               showToast(getString(R.string.toast_1))
+               true
+           }
+           R.id.item_menu_2 -> {
+               showToast(getString(R.string.toast_2))
+               true
+           }
+           R.id.item_menu_3 -> {
+               showToast(getString(R.string.toast_3))
+               true
+           }
+           else -> super.onOptionsItemSelected(item)
+       }
     }
 }
