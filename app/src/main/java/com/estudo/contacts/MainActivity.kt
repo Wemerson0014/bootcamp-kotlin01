f6ce7fcaa11c20f03f0f0817ae81a8c93aee1b4d
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-04-13 14:01:37 -0300

    Cria drawer layout, menus do drawer layout, recursos de strings para drawer layout, adiciona toolbar e modifica tema do aplicativo

--- app/src/main/java/com/estudo/contacts/MainActivity.kt
+++ app/src/main/java/com/estudo/contacts/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.estudo.contacts.model.Contact
@@ -16,10 +20,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
+        initDrawer()
         setupRecyclerView()
         updateList()
+    }
+
+    private fun initDrawer() {
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.main_toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.closed_drawer)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
     private fun setupRecyclerView() {
